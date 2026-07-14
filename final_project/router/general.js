52e2f3fedb8b8bcebcdb59585b9b4e088204efef
abelprisca <pria97491@gmail.com>
2026-07-14 23:09:35 +0100

    Completed Task 10-13 with Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,96 @@
 const express = require('express');
+const axios = require('axios'); // npm install axios
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Helper function: returns a Promise that resolves with books data
+let getBooks = () => {
+  return new Promise((resolve, reject) => {
+    if(books) {
+      resolve(books);
+    } else {
+      reject("No books available");
+    }
+  });
+}
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 10: Get the list of books using async-await + Promise
+public_users.get('/', async function (req, res) {
+  try {
+    const bookData = await getBooks();
+    return res.status(200).json(bookData);
+  } catch (error) {
+    return res.status(500).json({message: error});
+  }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 11: Get book details based on ISBN using async-await + Promise
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const isbn = req.params.isbn;
+  try {
+    const bookData = await getBooks();
+    if(bookData[isbn]) {
+      return res.status(200).json(bookData[isbn]);
+    } else {
+      return res.status(404).json({message: "Book not found"});
+    }
+  } catch (error) {
+    return res.status(500).json({message: error});
+  }
  });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+// Task 12: Get book details based on Author using async-await + Promise
+public_users.get('/author/:author', async function (req, res) {
+  const author = req.params.author;
+  try {
+    const bookData = await getBooks();
+    let booksByAuthor = [];
+    Object.keys(bookData).forEach((key) => {
+      if(bookData[key].author === author) {
+        booksByAuthor.push(bookData[key]);
+      }
+    });
+    if(booksByAuthor.length > 0) {
+      return res.status(200).json(booksByAuthor);
+    } else {
+      return res.status(404).json({message: "No books found for this author"});
+    }
+  } catch (error) {
+    return res.status(500).json({message: error});
+  }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 13: Get all books based on Title using async-await + Promise
+public_users.get('/title/:title', async function (req, res) {
+  const title = req.params.title;
+  try {
+    const bookData = await getBooks();
+    let booksByTitle = [];
+    Object.keys(bookData).forEach((key) => {
+      if(bookData[key].title === title) {
+        booksByTitle.push(bookData[key]);
+      }
+    });
+    if(booksByTitle.length > 0) {
+      return res.status(200).json(booksByTitle);
+    } else {
+      return res.status(404).json({message: "No books found with this title"});
+    }
+  } catch (error) {
+    return res.status(500).json({message: error});
+  }
 });
 
-//  Get book review
+// Task 5: Get book review - stays sync
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if(books[isbn]) {
+    res.send(JSON.stringify(books[isbn].reviews, null, 2));
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
